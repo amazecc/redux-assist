d89f431f10de1e5a7cb2bace18c5196c87e84357
Alex <cls1874@163.com>
2019-09-24 00:12:58 +0800

    fix bug

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -1,14 +1,23 @@
 type ProxyHandler = <T extends object>(obj: T, callback: (value: any) => T[keyof T]) => { before: T; after: T };
 
-const createError = () => new Error("[[ redux-assist caught exception]]: It is forbidden to set the variable of Module");
+const createError = (errorInfo: { target: any; key: string; value: any }) => {
+    return {
+        message: "[[ redux-assist caught exception]]: It is forbidden to set the variable of Module",
+        ...errorInfo
+    };
+};
 
 const proxyObjectHandler: ProxyHandler = (obj, callback) => {
     const after = new Proxy(obj, {
         get(target, key) {
             return callback.call(target, target[key as string]);
         },
-        set() {
-            throw createError();
+        set(target, key, value, receiver) {
+            // _pure_ created in file register.ts
+            if (key !== "_pure_") {
+                throw createError({ target, key: key as string, value });
+            }
+            return Reflect.set(target, key, value, receiver);
         }
     });
     return {
@@ -28,8 +37,8 @@ const definePropertyHandler: ProxyHandler = (obj, callback) => {
                 get() {
                     return callback.call(obj, obj[key]);
                 },
-                set() {
-                    throw createError();
+                set(value) {
+                    throw createError({ target: obj, key: key as string, value });
                 }
             });
         }
@@ -37,8 +46,8 @@ const definePropertyHandler: ProxyHandler = (obj, callback) => {
     // Its own property settings, the behavior is the same as the Proxy
     Object.keys(after).forEach(key => {
         Object.defineProperty(after, key, {
-            set() {
-                throw createError();
+            set(value) {
+                throw createError({ target: obj, key: key as string, value });
             }
         });
     });
